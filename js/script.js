a4a735c1d26bde49829a780c35f3a38dea7dc626
James Bremner <j.bremner@ymail.com>
2024-03-11 16:09:25 +0000

    add modal

--- js/script.js
+++ js/script.js
@@ -25,13 +25,52 @@ async function logMovies() {
         gallery.insertAdjacentHTML("beforeend", html)
     }
     createCard();
+
+    const employeeCards = document.querySelectorAll(".card");
+   const body = document.getElementsByTagName("body")[0];
+
+//loop through employee cards
+employeeCards.forEach(card => {
+    card.addEventListener("click", (event) => {
+      //display the modal 
+    //create modal
+    const popUp = document.createElement("div");
+    popUp.classList.add("modal-container");
+    //add html for modal
+    popUp.innerHTML += `<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`;
+    body.insertAdjacentElement("beforeend", popUp);
+    })
+})
   }
 
   logMovies();
 
-//display employee data
+
 
 //create pop up modal
+
+//capture click 
+//add event listener 
+
+
+const createModal = () => {
+  const modal = document.createElement("div");
+  
+}
+
+//create modal
 
 //add search functionality
 
